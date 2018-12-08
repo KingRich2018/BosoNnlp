2abf5bfbb52653d0b8d4765f6587bc15e5e989b4
KingRich2018 <37064071+KingRich2018@users.noreply.github.com>
2018-12-09 01:49:18 +0800

    Update BosoNnlpTest.java
    test

--- src/com/test/BosoNnlpTest.java
+++ src/com/test/BosoNnlpTest.java
@@ -65,7 +65,7 @@ public class BosoNnlpTest {
 	 */
 	@Test
 	public void classify() throws UnirestException {
-		String [] data =  new String[]{ "俄否决安理会谴责叙军战机空袭阿勒颇平民",
+		String [] data =  new String[]{ "否决安理会谴责叙军战机空袭阿勒颇平民",
 			    "邓紫棋谈男友林宥嘉：我觉得我比他唱得好",
 			    "Facebook收购印度初创公司"};
 		String str = Bosonnlp.determine(TypeBean.CLASSIFY.getDesc(),data);
